398a004cb911a0615ca8a05f9f96326f6bf020ad
scarletsurge <scarletsurge.m@gmail.com>
2024-11-22 17:28:41 +0300

    code from 22.11.24

--- include/binary_search_tree.h
+++ include/binary_search_tree.h
@@ -6,25 +6,34 @@ typedef int TValue;
 typedef int (*comparer)(TKey const*, TKey const*);
 typedef void (*value_handle_callback)(TValue const *);
 
-
 typedef struct bst_item
 {
     TKey key;
     TValue value;
     struct bst_item* left_subtree;
     struct bst_item* right_subtree;
-
-
 } bst_item, *p_bst_item, **pp_bst_item;
+
+typedef enum binary_search_tree_disposal_strategy
+{
+    swap_with_max_of_left_subtree,
+    swap_with_min_of_right_subtree
+} binary_search_tree_disposal_strategy;
 
 typedef struct binary_search_tree {
     p_bst_item root;
     comparer keys_comparer;
+    binary_search_tree_disposal_strategy disposal_strategy;
 } binary_search_tree, bst, *p_bst;
 
 int initialize_bst(
     p_bst bst_ptr,
-    comparer keys_comparer);
+    comparer keys_comparer,
+    binary_search_tree_disposal_strategy disposal_strategy);
+
+int set_disposal_strategy(
+    p_bst bst_ptr,
+    binary_search_tree_disposal_strategy disposal_strategy);
 
 int deinitialize_bst(
     p_bst bst_ptr);
@@ -35,12 +44,12 @@ int insert_into_bst(
     TValue const *value_to_insert_ptr);
 
 int obtain_from_bst(
-    p_bst const *bst_ptr,
+    binary_search_tree const *bst_ptr,
     TKey bst_key,
-    TValue ** bst_value);
+    TValue **bst_value_storage_ptr);
 
 int dispose_from_bst(
-    p_bst *bst_ptr,
+    p_bst bst_ptr,
     TKey bst_key);
 
 void bst_prefix_traversion_inner(
@@ -58,5 +67,7 @@ int bst_infix_traversion(
 int bst_postfix_traversion(
     bst const *bst_ptr,
     value_handle_callback callback);
+
+void bst_deinitialize_inner(p_bst_item);
 
 #endif //PROGRAMMINGSEMINARS_BINARY_SEARCH_TREE_H
--- src/binary_search_tree.c
+++ src/binary_search_tree.c
@@ -1,22 +1,68 @@
 #include "../include/binary_search_tree.h"
 #include <stdio.h>
 #include <malloc.h>
+
+#define OK 0
 #define UJASHO 1
 #define PYK_SRENK 2
+#define KEY_NOT_FOUND 3
 
-int initialize_bst(
-    p_bst bst_ptr,
-    comparer keys_comparer)
+int swap(
+    void *first,
+    void *second,
+    size_t bytes_to_swap_count)
 {
-    bst_ptr -> root = NULL;
-    bst_ptr -> keys_comparer = keys_comparer;
+    int i;
+    for (i = 0; i < bytes_to_swap_count; ++i)
+    {
+        *((unsigned char *)first + i) ^= *((unsigned char *)second + i);
+        *((unsigned char *)second + i) ^= *((unsigned char *)first + i);
+        *((unsigned char *)first + i) ^= *((unsigned char *)second + i);
+    }
+
     return 0;
 }
 
-int deinitialize_bst(
-    p_bst bst_ptr);
+int initialize_bst(
+    p_bst bst_ptr,
+    comparer keys_comparer,
+    binary_search_tree_disposal_strategy disposal_strategy)
 {
+    if (bst_ptr == NULL || keys_comparer == NULL)
+    {
+        return UJASHO;
+    }
 
+    bst_ptr->root = NULL;
+    bst_ptr->keys_comparer = keys_comparer;
+    bst_ptr->disposal_strategy = disposal_strategy;
+
+    return 0;
+}
+
+int set_disposal_strategy(
+    p_bst bst_ptr,
+    binary_search_tree_disposal_strategy disposal_strategy)
+{
+    if (bst_ptr == NULL)
+    {
+        return UJASHO;
+    }
+
+    bst_ptr->disposal_strategy = disposal_strategy;
+
+    return OK;
+}
+
+int deinitialize_bst(
+    p_bst bst_ptr)
+{
+    if (bst_ptr == NULL)
+        return UJASHO;
+
+    bst_deinitialize_inner(bst_ptr->root);
+    bst_ptr->root = NULL;
+    return OK;
 }
 
 int insert_into_bst(
@@ -59,18 +105,116 @@ int insert_into_bst(
 }
 
 int obtain_from_bst(
-    p_bst const *bst_ptr,
+    binary_search_tree const *bst_ptr,
     TKey bst_key,
-    TValue ** bst_value);
+    TValue **bst_value_storage_ptr)
+{
+    p_bst_item current_node_ptr;
+    int keys_comparison_result = 0;
+
+    if (bst_ptr == NULL)
+    {
+        return UJASHO;
+    }
+
+    if (bst_value_storage_ptr == NULL)
+    {
+        return UJASHO;
+    }
+
+    current_node_ptr = bst_ptr->root;
+
+    while (current_node_ptr != NULL)
+    {
+        keys_comparison_result = bst_ptr->keys_comparer(&bst_key, &current_node_ptr->key);
+
+        if (keys_comparison_result == 0)
+        {
+            *bst_value_storage_ptr = &current_node_ptr->value;
+
+            return OK;
+        }
+
+        current_node_ptr = keys_comparison_result > 0
+            ? current_node_ptr->right_subtree
+            : current_node_ptr->left_subtree;
+    }
+
+    return KEY_NOT_FOUND;
+}
 
 int dispose_from_bst(
-    p_bst const *bst_ptr,
-    TKey bst_key);
+    p_bst bst_ptr,
+    TKey bst_key)
+{
+    pp_bst_item current_node_ptr, for_swap_node_ptr;
+    p_bst_item temporary_node_ptr;
+    int keys_comparison_result = 0;
+
+    if (bst_ptr == NULL)
+    {
+        return UJASHO;
+    }
+
+    current_node_ptr = &bst_ptr->root;
+
+    while (current_node_ptr != NULL)
+    {
+        keys_comparison_result = bst_ptr->keys_comparer(&bst_key, &(*current_node_ptr)->key);
+
+        if (keys_comparison_result == 0)
+        {
+            if ((*current_node_ptr)->left_subtree != NULL &&
+                (*current_node_ptr)->right_subtree != NULL)
+            {
+                for_swap_node_ptr = bst_ptr->disposal_strategy == swap_with_max_of_left_subtree
+                                    ? &(*current_node_ptr)->left_subtree
+                                    : &(*current_node_ptr)->right_subtree;
+                while ((bst_ptr->disposal_strategy == swap_with_max_of_left_subtree
+                        ? (*for_swap_node_ptr)->right_subtree
+                        : (*for_swap_node_ptr)->left_subtree) != NULL)
+                {
+                    for_swap_node_ptr = bst_ptr->disposal_strategy == swap_with_max_of_left_subtree
+                                        ? &(*for_swap_node_ptr)->right_subtree
+                                        : &(*for_swap_node_ptr)->left_subtree;
+                }
+
+                swap((void *)&(*current_node_ptr)->key, (void *)&(*for_swap_node_ptr)->key, sizeof(TKey));
+                swap((void *)&(*current_node_ptr)->value, (void *)&(*for_swap_node_ptr)->value, sizeof(TValue));
+
+                current_node_ptr = for_swap_node_ptr;
+            }
+
+            if ((*current_node_ptr)->left_subtree == NULL &&
+                (*current_node_ptr)->right_subtree == NULL)
+            {
+                free(*current_node_ptr);
+                *current_node_ptr = NULL;
+            }
+            else
+            {
+                temporary_node_ptr = (*current_node_ptr)->left_subtree == NULL
+                    ? (*current_node_ptr)->right_subtree
+                    : (*current_node_ptr)->left_subtree;
+                free(*current_node_ptr);
+                *current_node_ptr = temporary_node_ptr;
+            }
+
+            return OK;
+        }
+
+        current_node_ptr = keys_comparison_result > 0
+            ? &(*current_node_ptr)->right_subtree
+            : &(*current_node_ptr)->left_subtree;
+    }
+
+    return KEY_NOT_FOUND;
+}
 
 void bst_prefix_traversion_inner(
     bst_item const* subtree,
-    void (*callback)(TValue const *)) {
-
+    void (*callback)(TValue const *))
+{
     if (subtree == NULL) {
         return;
     }
@@ -89,8 +233,26 @@ int bst_prefix_traversion(
 
 int bst_infix_traversion(
     bst const *bst_ptr,
-    void (*callback)(TValue const *));
+    void (*callback)(TValue const *))
+{
+
+}
 
 int bst_postfix_traversion(
     bst const *bst_ptr,
-    void (*callback)(TValue const *));
+    void (*callback)(TValue const *))
+{
+
+}
+
+void bst_deinitialize_inner(p_bst_item subtree)
+{
+    if (subtree == NULL)
+        return ;
+
+    bst_deinitialize_inner(subtree->left_subtree);
+
+    bst_deinitialize_inner(subtree->right_subtree);
+    free(subtree);
+
+}
