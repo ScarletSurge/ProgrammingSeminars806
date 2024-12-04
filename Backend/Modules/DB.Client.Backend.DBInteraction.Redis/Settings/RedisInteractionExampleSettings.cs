af779af98abe73152ee63e331e75d57a1179fb36
scarletsurge <scarletsurge.m@gmail.com>
2024-12-04 14:32:11 +0300

    hotfix

---
+++ Backend/Modules/DB.Client.Backend.DBInteraction.Redis/Settings/RedisInteractionExampleSettings.cs
@@ -0,0 +1,29 @@
+﻿namespace DB.Client.Backend.DBInteraction.Redis.Settings;
+
+/// <summary>
+/// 
+/// </summary>
+public sealed class RedisInteractionExampleSettings
+{
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Address
+    {
+        get;
+
+        set;
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public int Port
+    {
+        get;
+
+        set;
+    }
+    
+}
